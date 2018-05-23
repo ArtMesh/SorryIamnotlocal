187c253158c125efaa241b24c05bad66a0c73936
Iona <ionaprophet@gmail.com>
2018-05-23 22:42:34 +0300

    Initial commit

---
+++ app/src/main/java/com/artmesh/sorryiamnotlocal/MainActivity.java
@@ -0,0 +1,27 @@
+package com.artmesh.sorryiamnotlocal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
